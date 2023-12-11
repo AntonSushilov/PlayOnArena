f63160bc8dfdf371fbf08b75d6c51ddc92d762b1
Соловьёв Андрей Андреевич <groozzy@gmail.com>
2023-12-12 00:22:50 +0300

    Fix(settings): fix media_root

--- backend/play_on_arena/urls.py
+++ backend/play_on_arena/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -26,8 +27,7 @@ from play_on_arena import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
