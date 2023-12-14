f589bbe7851fb813fef553b5b6d2fc3b03bfa1d9
Соловьёв Андрей Андреевич <groozzy@gmail.com>
2023-12-14 23:49:57 +0300

    Fix(tournaments): fix management

--- backend/tournaments/management/commands/upload_city.py
+++ backend/tournaments/management/commands/upload_city.py
@@ -2,11 +2,13 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from tournaments.models import City
+from tournaments.models import City, Country
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open('data/Country.json', encoding='utf-8') as data:
+        with open('data/City.json', encoding='utf-8') as data:
             for city in json.loads(data.read()):
-                City.objects.get_or_create(**city)
+                country_id = city.pop('country')
+                country = Country.objects.get(id=country_id)
+                City.objects.get_or_create(**city, country=country)
